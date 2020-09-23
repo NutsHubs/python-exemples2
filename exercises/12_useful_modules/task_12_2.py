918b8c5d99d264c119a27ec6f4987e6939e6530a
Abysscope <abysscope@gmail.com>
2020-09-23 16:59:56 +0300

    task12 complete

--- exercises/12_useful_modules/task_12_2.py
+++ exercises/12_useful_modules/task_12_2.py
@@ -30,3 +30,80 @@
  '172.21.41.129', '172.21.41.130', '172.21.41.131', '172.21.41.132']
 
 """
+
+import argparse
+import ipaddress
+import subprocess
+
+
+def check_ip_address(ip):
+    result = list()
+    try:
+        for ip in args.ip:
+            result.append(ipaddress.ip_address(ip))
+    except ValueError as e:
+        print('Wrong ip addresse(s). Enter correct ip addresse(s)', e)
+        raise SystemExit
+    return result
+
+
+def ping_ip_addresses(ip_list):
+    success = list()
+    wrong = list()
+
+    for ip in ip_list:
+        reply = subprocess.run(['ping', str(ip), '-c', '4'], stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE, encoding='utf-8')
+        if reply.returncode == 0:
+            success.append(str(ip))
+        else:
+            wrong.append(str(ip))
+
+    result = (success, wrong)
+
+    return result
+
+
+def convert_ranges_to_ip_list(ip_list):
+    """
+    Convert ranges ip addresses
+    by format a.b.c.d-a.b.c.n
+    or a.b.c.d-n
+    in list of particular ip addresses
+    """
+    result = list()
+
+    for ip_range in ip_list:
+        a = int(str(ip_range).find('-'))
+        if a > 1:
+            ip_range_split = str(ip_range).split('-')
+            if ip_range_split[1].count('.') == 3:
+                rangestart, rangestop = ip_range_split[0].split('.')[3], \
+                                        ip_range_split[1].split('.')[3]
+            else:
+                rangestart, rangestop = ip_range_split[0].split('.')[3], \
+                                        ip_range_split[1]
+
+            ipstart = ip_range_split[0][:ip_range_split[0].rfind('.') + 1]
+            for ipiter in range(int(rangestart), int(rangestop) + 1):
+                result.append("{}{}".format(ipstart, ipiter))
+        else:
+            result.append(ip_range)
+
+    return result
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Script check IP addresses')
+    parser.add_argument('ip', nargs='+', help='IP addresse(s) by format a.b.c.d or range a.b.c.d-n or a.b.c.d-a,b,c,n')
+    args = parser.parse_args()
+
+    args.ip = ['10.1.1.1', '10.4.10.10-13', '192.168.1.12-192.168.1.15']
+
+
+    print(convert_ranges_to_ip_list(args.ip))
+
+
+    #ipaddress_list = check_ip_address(ip_list)
+
+    #print(ping_ip_addresses(ipaddress_list))
--- exercises/12_useful_modules/task_12_3.py
+++ exercises/12_useful_modules/task_12_3.py
@@ -23,3 +23,69 @@ Reachable    Unreachable
 
 Для этого задания нет тестов
 """
+
+import argparse
+import ipaddress
+import subprocess
+from  tabulate import tabulate
+
+
+def checkipaddr(ip):
+    result = list()
+    try:
+        for ip in args.ip:
+            result.append(ipaddress.ip_address(ip))
+    except ValueError as e:
+        print('Wrong ip addresse(s). Enter correct ip addresse(s)')
+        raise SystemExit
+    return result
+
+
+def ping_ip_addresses(ip_list):
+    success = list()
+    wrong = list()
+
+    for ip in ip_list:
+        reply = subprocess.run(['ping', str(ip), '-c', '4'], stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE, encoding='utf-8')
+        if reply.returncode == 0:
+            success.append(str(ip))
+        else:
+            wrong.append(str(ip))
+
+    result = (success, wrong)
+
+    return result
+
+
+def print_ip_table(reachable_list, unreachable_list):
+
+    length_reach = len(reachable_list)
+    length_unreached = len(unreachable_list)
+
+    reach_col = list(reachable_list)
+    unreached_col = list(unreachable_list)
+
+    if length_reach > length_unreached:
+        diff = length_reach - length_unreached
+        diff_list = [' ']*diff
+        unreached_col.extend(diff_list)
+    elif length_unreached > length_reach:
+        diff = length_unreached - length_reach
+        diff_list = [' '] * diff
+        reach_col.extend(diff_list)
+
+    print(tabulate(zip(reach_col, unreached_col), ['reachable', 'unreachable'], ))
+
+    print(reachable_list, unreachable_list)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Script check IP addresses')
+    parser.add_argument('ip', nargs='+', help='IP addresse(s) by format a.b.c.d')
+    args = parser.parse_args()
+
+    #ipaddress_list = checkipaddr(args.ip)
+    #pinged_addresses = ping_ip_addresses(ipaddress_list)
+    #print(pinged_addresses)
+    print_ip_table(['10.1.1.1', '10.1.1.2'], ['12.1.1.1', '12.1.1.2', '12.1.1.3'])
